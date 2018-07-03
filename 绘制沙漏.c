962387d65e062d311c8b20825ea85f3ede240133
velenk <38948738+velenk@users.noreply.github.com>
2018-07-03 23:00:29 +0800

    ...

--- 绘制沙漏.c
+++ 绘制沙漏.c
@@ -5,20 +5,26 @@ void draw(int i,char s);
 int main()
 {
   char s;
-  int n;
+  int n,x;
   int i = 1;
   
-  scanf("%d %c",&n,&s);
+  if(scanf("%d %c",&n,&s)){
+  }
   
   while((i*i*2-1)<n){
     i++;
   }
   
   i--;
-  int x = n - (i*i*2-1);
-  
-  draw(i,s);
-  printf("%d",x);
+  if(i==0){
+  	x = n - 1;
+  	printf("%c\n",s);
+  }else{
+  	x = n - (i*i*2-1);
+  	draw(i,s);
+  }
+
+  printf("%d\n",x);
   
   return 0;
 }
@@ -42,8 +48,8 @@ void draw(int i,char s)
   }
   
   int max = cnt0 + 1;
-  i = 1;
-  cnt0--;
+  i = 2;
+  cnt0 -= 2;
   
   for(;i<max;i++){
   	cnt = 2*i-1;
