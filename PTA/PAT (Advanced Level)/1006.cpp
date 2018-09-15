0aaaf8cfe51909257693a385e6fc83a7e521de38
velenk <38948738+velenk@users.noreply.github.com>
2018-09-15 16:25:30 +0800

    .

---
+++ PTA/PAT (Advanced Level)/1006.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Student {
+  string id;
+  string in;
+  string out;
+};
+
+int main() {
+  int n;
+  Student student;
+  Student in;
+  Student out;
+  cin >> n;
+  cin >> student.id >> student.in >> student.out;
+  in = out = student;
+  for (; n > 0; --n) {
+    cin >> student.id >> student.in >> student.out;
+    if (in.in > student.in) {
+      in = student;
+    }
+    if (out.out < student.out) {
+      out = student;
+    }
+  }
+  cout << in.id << " " << out.id << endl;
+  return 0;
+}
