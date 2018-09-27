321c8de6b914161c4fb76a947d98f5e503c8325d
velenk <38948738+velenk@users.noreply.github.com>
2018-09-28 01:41:46 +0800

    .

--- PTA/PAT (Advanced Level)/1010.cpp
+++ PTA/PAT (Advanced Level)/1010.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+//#define _TEST_
+//#define _TEST_1_
 using namespace std;
 
 int CharToInt(char c) {
@@ -11,8 +13,13 @@ int CharToInt(char c) {
 
 long long ArrayToLonglong(char a[], int radix) {
   int i, sum = 0;
+  int num;
   for (i = 0; a[i] != '\0'; ++i) {
-    sum += sum * radix + CharToInt(a[i]);
+    num = CharToInt(a[i]);
+    sum = sum * radix + num;
+    #ifdef _TEST_1_
+    cout << num << " " << sum << endl;
+    #endif
   }
   return sum;
 }
@@ -27,29 +34,43 @@ long long GetSmallestRadix(char a[]) {
       max = num;
     }
   }
-  return max + 1;
+  return max;
 }
 
 long long FindRadix(char a[], char b[], long long radix) {
-  long long begin = GetSmallestRadix(a);
+  long long begin = GetSmallestRadix(b);
   long long end = ArrayToLonglong(a, radix);
   long long number = end;
   long long mid, now;
-  while (begin < end) {
+  #ifdef _TEST_
+  cout << number << " " << begin << " " << end << endl;
+  #endif
+  while (begin <= end) {
     mid = (begin + end) / 2;
     now = ArrayToLonglong(b, mid);
+    #ifdef _TEST_
+    cout << now << " ";
+    #endif
     if (now > number) {
-      end = mid;
+      end = mid - 1;
     } else if (now < number) {
-      begin = mid;
+      begin = mid + 1;
     } else {
       return mid;
     }
+    #ifdef _TEST_
+    cout << begin << " " << end << endl;
+    #endif
   }
   return -1;
 }
 
 int main() {
+  #ifdef _TEST_1_
+  char x[4] = "110";
+  long long x0 = ArrayToLonglong(x, 2);
+  cout << x0 << endl;
+  #endif
   char a[11] = { }, b[11] = { };
   int tag;
   long long radix;
@@ -60,9 +81,19 @@ int main() {
       result = FindRadix(a, b, radix);
       break;
     case 2:
+      #ifdef _TEST_
+      cout << 2 << endl;
+      #endif
       result = FindRadix(b, a, radix);
       break;
   }
-  cout << result;
+  #ifdef _TEST_
+  cout << endl << endl;
+  #endif
+  if (result == -1) {
+    cout << "Impossible";
+  } else {
+    cout << result;
+  }
   return 0;
 }
