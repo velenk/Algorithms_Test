6d806bfb02e8a923b207b781631e0a0b571cf9ac
velenk <38948738+velenk@users.noreply.github.com>
2018-10-26 19:36:17 +0800

    .

---
+++ 洛谷/1085.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, i, k = 0, sum = 0;
+    for (i = 0; i < 7; ++i) {
+        cin >> a >> b;
+        if (a + b > 8 && a + b > sum) {
+            k = i;
+            sum = a + b;
+        }
+    }
+    cout << k;
+    return 0;
+}
